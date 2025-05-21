f4f32b9ff84baf3e0d229551741ab27784233ca2
DEVILOCHEEK <123664311+DEVILOCHEEK@users.noreply.github.com>
2025-05-21 21:44:07 +0300

    Create event_consumer.py

---
+++ kafka/event_consumer.py
@@ -0,0 +1,5 @@
+# kafka/event_consumer.py
+from kafka import KafkaConsumer
+consumer = KafkaConsumer('user-actions', bootstrap_servers='localhost:9092')
+for msg in consumer:
+    print("Event received:", msg.value.decode())
